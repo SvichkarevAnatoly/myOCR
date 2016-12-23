7b305e9b5304d6e6dd1e66b87a9828a15838f779
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-12-23 21:39:41 +0300

    реализация алгоритма для сравнения строк

--- src/main/java/align/SimpleAlignment.java
+++ src/main/java/align/SimpleAlignment.java
@@ -5,7 +5,13 @@ public class SimpleAlignment {
     private int deletion;
     private int mismatch;
 
-    private int scoreMatrix[][];
+    private String str1;
+    private String str2;
+
+    private int s1Len;
+    private int s2Len;
+
+    private int score[][];
     private char backtrack[][];
 
     public SimpleAlignment(int deletion, int mismatch) {
@@ -14,16 +20,83 @@ public class SimpleAlignment {
     }
 
     public int align(String string1, String string2) {
-        final int s1Len = string1.length();
-        final int s2Len = string2.length();
+        str1 = string1;
+        str2 = string2;
+        s1Len = string1.length();
+        s2Len = string2.length();
 
-        scoreMatrix = new int[s1Len + 1][];
-        for (int i = 0; i < s1Len + 1; i++) {
-            scoreMatrix[i] = new int[s2Len + 1];
+        initMatrices();
+        fillMatrices();
+
+        return score[s2Len][s1Len];
+    }
+
+    public void initMatrices() {
+        score = initIntMatrix();
+        backtrack = initCharMatrix();
+
+        initBoarders();
+    }
+
+    private int[][] initIntMatrix() {
+        final int[][] matrix = new int[s2Len + 1][];
+        for (int i = 0; i < s2Len + 1; i++) {
+            matrix[i] = new int[s1Len + 1];
         }
 
-        // TODO: доделать
-        
-        return 0;
+        return matrix;
+    }
+
+    private char[][] initCharMatrix() {
+        final char[][] matrix = new char[s2Len][];
+        for (int i = 0; i < s2Len; i++) {
+            matrix[i] = new char[s1Len];
+        }
+
+        return matrix;
+    }
+
+    private void initBoarders() {
+        for (int i = 1; i <= s2Len; i++) {
+            score[i][0] = -deletion * i;
+        }
+        for (int i = 1; i <= s1Len; i++) {
+            score[0][i] = -deletion * i;
+        }
+    }
+
+    private void fillMatrices() {
+        fillScoreMatrix();
+        fillBacktrack();
+    }
+
+    private void fillScoreMatrix() {
+        for (int i = 1; i < s2Len + 1; i++) {
+            for (int j = 1; j < s1Len + 1; j++) {
+                final int up = score[i - 1][j] - deletion;
+                final int left = score[i][j - 1] - deletion;
+
+                final int adding = str2.charAt(i - 1) == str1.charAt(j - 1) ? 1 : -mismatch;
+                final int diag = score[i - 1][j - 1] + adding;
+
+                final int maxUpLeft = Math.max(up, left);
+                score[i][j] = Math.max(maxUpLeft, diag);
+            }
+        }
+    }
+
+    private void fillBacktrack() {
+        for (int i = 1; i < s2Len + 1; i++) {
+            for (int j = 1; j < s1Len + 1; j++) {
+                if (score[i][j] == score[i - 1][j] - deletion) {
+                    // TODO: вынести в константы
+                    backtrack[i - 1][j - 1] = '↓';
+                } else if (score[i][j] == score[i][j-1]){
+                    backtrack[i - 1][j - 1] = '→';
+                } else {
+                    backtrack[i - 1][j - 1] = '↘';
+                }
+            }
+        }
     }
 }
