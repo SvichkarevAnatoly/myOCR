368ee1845d4a50bd694598a2d67d6147b3101c79
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-12-24 14:55:05 +0300

    тест на поиск в выборке курицы

--- src/test/java/align/SimpleAlignmentRealDataTest.java
+++ src/test/java/align/SimpleAlignmentRealDataTest.java
@@ -6,27 +6,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class SimpleAlignmentRealDataTest {
-    @Test
-    public void chicken() throws Exception {
-        final String ocr = "[114ЛЕ Б/К0ЖИ К9РИН0Е 0ХЛ. НА П0187. ММ 187. И";
-        final String real = "ФИЛЕ Б/КОЖИ КУРИНОЕ ОХЛ. НА ПО187. 14*1   187. 14";
-        final SimpleAlignment alignment = new SimpleAlignment();
-
-        System.out.println("score = " + alignment.align(ocr, real));
-        assertThat(alignment.align(ocr, real), is(17));
-
-        // alignment.printScoreMatrix();
-        // alignment.printBacktrack();
-
-        System.out.println(alignment.getAlignString1());
-        System.out.println(alignment.getAlignString2());
-
-        final String expectedAlignStr1 = "[114ЛЕ Б/К0ЖИ К9РИН0Е 0ХЛ. НА П0187. ММ-- --187. И-";
-        final String expectedAlignStr2 = "ФИ--ЛЕ Б/КОЖИ КУРИНОЕ ОХЛ. НА ПО187. 14*1   187. 14";
-
-        assertThat(alignment.getAlignString1(), is(expectedAlignStr1));
-        assertThat(alignment.getAlignString2(), is(expectedAlignStr2));
-    }
+    private final String ocrChicken = "[114ЛЕ Б/К0ЖИ К9РИН0Е 0ХЛ. НА П0187. ММ 187. И";
+    final String expectedChickenAlignment = "ФИ--ЛЕ Б/КОЖИ КУРИНОЕ ОХЛ. НА ПО187. 14*1 187. 14";
 
     @Test
     public void chickenWord() throws Exception {
@@ -34,7 +15,7 @@ public class SimpleAlignmentRealDataTest {
         final String real = "КУРИНОЕ";
         final SimpleAlignment alignment = new SimpleAlignment();
 
-        assertThat(alignment.align(ocr, real), is(498));
+        assertThat(alignment.align(ocr, real), is(3));
 
         System.out.println(alignment.getAlignString1());
         System.out.println(alignment.getAlignString2());
@@ -44,5 +25,53 @@ public class SimpleAlignmentRealDataTest {
 
         assertThat(alignment.getAlignString1(), is(ocr));
         assertThat(alignment.getAlignString2(), is(real));
+    }
+
+    @Test
+    public void chickenFull() throws Exception {
+        final String real = "ФИЛЕ Б/КОЖИ КУРИНОЕ ОХЛ. НА ПО187. 14*1 187. 14";
+        final SimpleAlignment alignment = new SimpleAlignment();
+
+        System.out.println("score = " + alignment.align(ocrChicken, real));
+        assertThat(alignment.align(ocrChicken, real), is(19));
+
+        // alignment.printScoreMatrix();
+        // alignment.printBacktrack();
+
+        System.out.println(alignment.getAlignString1());
+        System.out.println(alignment.getAlignString2());
+
+        final String expectedAlignStr1 = "[114ЛЕ Б/К0ЖИ К9РИН0Е 0ХЛ. НА П0187. ММ-- 187. И-";
+
+        assertThat(alignment.getAlignString1(), is(expectedAlignStr1));
+        assertThat(alignment.getAlignString2(), is(expectedChickenAlignment));
+    }
+
+    @Test
+    public void findChicken() throws Exception {
+        int maxScore = Integer.MIN_VALUE;
+        String maxScoreFoodName = "empty";
+
+        final SimpleAlignment alignment = new SimpleAlignment();
+        for (String foodName : TestRealData.food30Names) {
+            final int score = alignment.align(ocrChicken, foodName);
+            System.out.println("score = " + score);
+            System.out.println(alignment.getAlignString1());
+            System.out.println(alignment.getAlignString2());
+            System.out.println();
+
+            if (score > maxScore) {
+                maxScore = score;
+                maxScoreFoodName = foodName;
+            }
+        }
+
+        System.out.println("--------------------------------------");
+        final int score = alignment.align(ocrChicken, maxScoreFoodName);
+        System.out.println("score = " + score);
+        System.out.println(alignment.getAlignString1());
+        final String maxScoreAlignment = alignment.getAlignString2();
+        System.out.println(maxScoreAlignment);
+        assertThat(maxScoreAlignment, is(expectedChickenAlignment));
     }
 }
