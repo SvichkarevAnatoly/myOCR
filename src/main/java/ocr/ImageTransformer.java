27c736d96a566cde6b5be2d453953af3e4af922d
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-01-01 18:18:32 +0300

    добавление метода перевода в оттенки серого

--- src/main/java/ocr/ImageTransformer.java
+++ src/main/java/ocr/ImageTransformer.java
@@ -3,15 +3,19 @@ package ocr;
 import org.bytedeco.javacpp.opencv_core.CvSize;
 import org.bytedeco.javacpp.opencv_core.IplImage;
 
-import static org.bytedeco.javacpp.opencv_core.cvCreateImage;
-import static org.bytedeco.javacpp.opencv_core.cvSize;
+import static org.bytedeco.javacpp.opencv_core.*;
+import static org.bytedeco.javacpp.opencv_imgproc.CV_BGR2GRAY;
+import static org.bytedeco.javacpp.opencv_imgproc.cvCvtColor;
 import static org.bytedeco.javacpp.opencv_imgproc.cvResize;
 
 public class ImageTransformer {
+    // оригинал не изменяется
     private final Image image;
+    private final IplImage iplImage;
 
     public ImageTransformer(Image image) {
         this.image = image;
+        iplImage = this.image.getImage();
     }
 
     public Image scale(int percent) {
@@ -26,5 +30,13 @@ public class ImageTransformer {
 
         final Image scaledImage = new Image(scaledIplImage);
         return scaledImage;
+    }
+
+    public Image toGray() {
+        final IplImage grayIplImage = cvCreateImage(iplImage.cvSize(), IPL_DEPTH_8U, 1);
+        cvCvtColor(iplImage, grayIplImage, CV_BGR2GRAY);
+
+        final Image grayImage = new Image(grayIplImage);
+        return grayImage;
     }
 }
