5d3364d98088412dc770e7c46ac7e0fab10b8219
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-01-01 15:18:47 +0300

    выделение класса для работы с изображением

---
+++ src/main/java/ocr/Image.java
@@ -0,0 +1,28 @@
+package ocr;
+
+import org.bytedeco.javacpp.opencv_core.IplImage;
+
+import java.io.File;
+
+import static org.bytedeco.javacpp.opencv_imgcodecs.cvLoadImage;
+import static org.bytedeco.javacpp.opencv_imgcodecs.cvSaveImage;
+
+public class Image {
+    private IplImage iplImage;
+
+    public Image() {}
+
+    public Image(File imageFile) {
+        load(imageFile);
+    }
+
+    public void load(File imageFile) {
+        final String imageFilePath = imageFile.getAbsolutePath();
+        iplImage = cvLoadImage(imageFilePath);
+    }
+
+    public void save(File file) {
+        final String imageFilePath = file.getAbsolutePath();
+        cvSaveImage(imageFilePath, iplImage);
+    }
+}
